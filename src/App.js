705426a4ffb99e07f70fd06a9eb00307cf639924
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2020-02-28 00:41:06 -0600

    final-02272020

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import "./styles.css";
 import Person from './Person/Person';
 
 export default function App() {
+  state = {
+    persons:[
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26},
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked');
+  }
+
   return (
     <div className="App">
       <h1 className="test">This is a test!</h1>
       <p>How are we doing?</p>
-      <Person />
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
   );
   // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now?'))
