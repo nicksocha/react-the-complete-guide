6411d7fed3d6ad057ba7a062da66c543e63ad104
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2020-03-03 00:31:29 -0600

    final-03032020

--- src/App.js
+++ src/App.js
@@ -1,29 +1,102 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import './App.css';
 import Person from './Person/Person';
 
-export default function App() {
+const StyledButton = styled.button`
+  background: ${props =>
+    props.alt
+      ? 'linear-gradient(to bottom, #adebad 5%, #999999 100%)'
+      : 'linear-gradient(to bottom, #999999 5%, #ff99c2 100%)'};
+  border-radius: 15px;
+  border: none;
+  box-shadow: 0px 3px 5px 0px #ffc600;
+  color: #000;
+  cursor: pointer;
+  display: inline-block;
+  font-family: BioRhyme;
+  font-size: 1.5vw;
+  outline: none;
+  padding: 7px 25px;
+  text-decoration: none;
+  &:hover {
+    background: ${props =>
+      props.alt
+        ? 'linear-gradient(to bottom, #84e184 5%, #999999 100%)'
+        : 'linear-gradient(to bottom, #999999 5%, #ff66a3 100%)'};
+  }
+`;
+
+class App extends Component {
   state = {
-    persons:[
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26},
-    ]
-  }
+    persons: [
+      { id: '1', name: 'Max', age: 28 },
+      { id: '2', name: 'Manu', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 },
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+  };
 
-  switchNameHandler = () => {
-    console.log('was clicked');
-  }
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons });
+  };
 
-  return (
-    <div className="App">
-      <h1 className="test">This is a test!</h1>
-      <p>How are we doing?</p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-    </div>
-  );
-  // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now?'))
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    person[personIndex] = person;
+    this.setState({ persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => (
+            <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={event => this.nameChangedHandler(event, person.id)}
+            />
+          ))}
+        </div>
+      );
+    }
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+    return (
+      <div className="App">
+        <h1>WOW!!! This is React!!!</h1>
+        <p className={classes.join(' ')}>It is really working! Kinda...</p>
+        <StyledButton
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
+          Toggle Persons
+        </StyledButton>
+        <p>Maybe...</p>
+        {persons}
+      </div>
+    );
+  }
 }
+
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import './Person.css';
+import styled from 'styled-components';
 
-const person = (props) => {
-    return (
-    <div>
-        <p>I'm {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-    </div>
-    )
+const StyledDiv = styled.div`
+  border: 2px solid #e3e3e3;
+  border-radius: 1.5vw;
+  box-shadow: 0 3px 5px #ffc600;
+  font-family: 'Fira Code';
+  margin: 2vh auto;
+  padding: 1rem;
+  max-width: 60vw;
+`;
+
+const person = props => {
+  return (
+    <StyledDiv>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </StyledDiv>
+  );
 };
 
 export default person;
